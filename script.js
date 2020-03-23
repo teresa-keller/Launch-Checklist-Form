caa3657fd0246fa15f580742cf4a2fba4421990a
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-03-23 15:33:13 -0500

    part one

--- script.js
+++ script.js
@@ -11,3 +11,33 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function() {
+
+
+let launchForm = document.getElementById("launchForm"); 
+let pilotName = document.querySelector("input[name = pilotName]");
+let fuelLevel = document.querySelector("input[name = fuelLevel]");
+let cargoMass = document.querySelector("input[name = cargoMass]");
+let copilotName = this.document.querySelector("input[name = copilotName]");
+
+
+   this.event.preventDefault();
+   this.console.log("yep");
+
+// this.console.log(pilotName.value);
+// let formSubmitButton = document.getElementById("formSubmit");
+launchForm.addEventListener("submit", function() {
+   // let pilotNameInput = document.querySelector("input[name=pilotName]");
+   // let pilotName = document.getElementById("pilotName");
+   // console.log(pilotNameInput);
+   // if (typeof pilotNameInput.value !== "string") {
+      // alert("Please enter a valid name.");
+   // }
+   if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value ==="") {
+      alert("All fields required!");
+   
+   }
+
+   });
+});
