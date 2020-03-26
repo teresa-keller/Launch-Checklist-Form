8422a01a5502078affe9abf0ea2ca600a8ce744a
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-03-25 20:11:48 -0500

    added faulty fuel and cargo

--- script.js
+++ script.js
@@ -21,16 +21,19 @@ let fuelLevel = this.document.querySelector("input[name = fuelLevel]");
 let cargoMass = this.document.querySelector("input[name = cargoMass]");
 let copilotName = this.document.querySelector("input[name = copilotName]");
 let faultyItems = this.document.getElementById("faultyItems");
-fuelLevel = fuelStatus;
+let pilotStatus = document.getElementById("pilotStatus");
+let copilotStatus = this.document.getElementById("copilotStatus");
+let fuelStatus = this.document.getElementById("fuelStatus");
+let cargoStatus = this.document.getElementById("cargoStatus");
    console.log(faultyItems);
-   // console.log(faultyItems.fuelStatus);
-
+   
    
    this.console.log("yep");
-// this.console.log(copilotName.value);
+
 
 launchForm.addEventListener("submit", function() {
    event.preventDefault();
+   // pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
    
    if (isNaN(fuelLevel.value)) {
       alert("Please enter a number.");
@@ -40,17 +43,25 @@ launchForm.addEventListener("submit", function() {
       alert("Please enter a number.");
    }
    
+   if(fuelLevel.value < 10000) {
+      faultyItems.style.visibility = "visible";
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready.`;
+      copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready.`;
+      fuelStatus.innerHTML = "Fuel level too low for launch.";
+   } 
+   
+   if(cargoMass.value > 10000) {
+      faultyItems.style.visibility = "visible";
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready.`;
+      copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready.`;
+      cargoStatus.innerHTML = "Cargo mass too high for launch.";
+   }
+   
+   
+   
    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value ==="") {
       alert("All fields required!");   
    }
-   // console.log(fuelStatus);
-   if (fuelStatus.value < 10000) {
-      // fuelStatus.append("Fuel level too low!");
-   // }
-   // getElementById("fuelStatus").addEventListener("submit", function() {
-      fuelStatus.innerHTML = "Fuel level too low!";
-      // });
-      }   
 
    });
 });
