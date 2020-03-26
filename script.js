8b575f717f83d1d1408cb0e932c5d9679b0aaec4
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-03-25 19:43:05 -0500

    alerts for numbers

--- script.js
+++ script.js
@@ -15,25 +15,42 @@
 window.addEventListener("load", function() {
 
 
-let launchForm = document.getElementById("launchForm"); 
-let pilotName = document.querySelector("input[name = pilotName]");
-let fuelLevel = document.querySelector("input[name = fuelLevel]");
-let cargoMass = document.querySelector("input[name = cargoMass]");
+let launchForm = this.document.getElementById("launchForm"); 
+let pilotName = this.document.querySelector("input[name = pilotName]");
+let fuelLevel = this.document.querySelector("input[name = fuelLevel]");
+let cargoMass = this.document.querySelector("input[name = cargoMass]");
 let copilotName = this.document.querySelector("input[name = copilotName]");
+let faultyItems = this.document.getElementById("faultyItems");
+fuelLevel = fuelStatus;
+   console.log(faultyItems);
+   // console.log(faultyItems.fuelStatus);
 
-
-   this.event.preventDefault();
+   
    this.console.log("yep");
-// console.log(Number(cargoMass.value));
+// this.console.log(copilotName.value);
 
 launchForm.addEventListener("submit", function() {
-   if(typeof pilotName.value !== "string") {
-      alert("Please enter a valid name.");
-   }   
-   // console.log(typeof pilotName.value);
+   event.preventDefault();
+   
+   if (isNaN(fuelLevel.value)) {
+      alert("Please enter a number.");
+   }
+
+   if(isNaN(cargoMass.value)) {
+      alert("Please enter a number.");
+   }
+   
    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value ==="") {
       alert("All fields required!");   
    }
+   // console.log(fuelStatus);
+   if (fuelStatus.value < 10000) {
+      // fuelStatus.append("Fuel level too low!");
+   // }
+   // getElementById("fuelStatus").addEventListener("submit", function() {
+      fuelStatus.innerHTML = "Fuel level too low!";
+      // });
+      }   
 
    });
 });
