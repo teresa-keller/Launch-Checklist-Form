21b13055eaf84b4a9db234d023b7ac9c808158e3
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-06-18 12:31:06 -0500

    added random selection

--- script.js
+++ script.js
@@ -16,15 +16,17 @@ window.addEventListener("load", function() {
 
    this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
       response.json().then( function(json) {
+         let index = Math.floor(Math.random() * json.length);
+
          missionTarget.innerHTML = `<h2>Mission Destination</h2>
          <ol>
-            <li>Name: ${json[0].name}</li>
-            <li>Diameter: ${json[0].diameter}</li>
-            <li>Star: ${json[0].star}</li>
-            <li>Distance from Earth: ${json[0].distance}</li>
-            <li>Number of Moons: ${json[0].moons}</li>
+            <li>Name: ${json[index].name}</li>
+            <li>Diameter: ${json[index].diameter}</li>
+            <li>Star: ${json[index].star}</li>
+            <li>Distance from Earth: ${json[index].distance}</li>
+            <li>Number of Moons: ${json[index].moons}</li>
          </ol>
-         <img src="${json[0].image}">
+         <img src="${json[index].image}">
          `;
       });
    });
